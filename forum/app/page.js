a26a84ce8051d8a4a7cc592933764f4c551d7836
Leeseoleem <leeseorim0029@gmail.com>
2025-03-13 23:48:16 +0900

    feat project start

---
+++ forum/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col p-12">
+      <p className="text-[60px] font-thin">저는 뭘까요...</p>
+      <p className="text-[60px] font-medium">저는 뭘까요...</p>
+      <p className="text-[60px] font-bold">저는 뭘까요...</p>
+      <p className="text-[60px] font-black">저는 뭘까요...</p>
+    </div>
+  );
+}
